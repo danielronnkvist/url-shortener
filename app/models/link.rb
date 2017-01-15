3a98e1b8a5bfa4909b19869e2d9fd39ddd554778
Daniel Rönnkvist <daniel@ronnkvist.com>
2017-01-14 16:25:40 -0800

    Generate a shortened string on link create

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,14 @@
 class Link < ApplicationRecord
   belongs_to :user
   validates :original, :user_id, presence: true
+  validates :shortened, uniqueness: true
+  before_create :generate_shortened
+
+  private
+
+  def generate_shortened
+    begin
+      self.shortened = SecureRandom.hex(self.class.count + 1).downcase
+    end while self.class.exists?(shortened: shortened)
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Link, type: :model do
   it "should create a new instance of a link given valid attributes" do
     Link.create!(@link)
   end
+
+  it "should not create a new instance of a link given no user" do
+    @link['user'] = nil
+    expect{Link.create!(@link)}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should not create a new instance of a link given no original" do
+    @link['original'] = nil
+    expect{Link.create!(@link)}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should generate a unique shortened url after create" do
+    link = Link.create(@link)
+    expect(Link.where(shortened: link.shortened)).to eq([link])
+  end
 end
