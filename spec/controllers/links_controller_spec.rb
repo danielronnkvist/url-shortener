326ecd29cf670267bf0b79e1f332f3b0e5c57002
Daniel Rönnkvist <daniel@ronnkvist.com>
2017-01-16 17:51:07 -0800

    Make sure the links controller can't be visited without authentication

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe LinksController, type: :controller do
       end
     end
   end
+
+  it "shouldn't be able to visit without being signed in" do
+    get :index
+    expect(response).to redirect_to(new_user_session_path)
+  end
 end
